02058fce6722b5e6dab4059eb255878715d9ea36
niv cohen <niv2@post.bgu.ac.il>
2022-02-20 16:18:48 +0200

    session doesn't work4

--- pages/contact_us/contact_us.py
+++ pages/contact_us/contact_us.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from utilities.db.contact_db import DBcontacts
+
 
 # about blueprint definition
 contact_us = Blueprint('contact_us', __name__, static_folder='static', static_url_path='/contact_us', template_folder='templates')
@@ -8,6 +10,10 @@ contact_us = Blueprint('contact_us', __name__, static_folder='static', static_ur
 @contact_us.route('/contact_us', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        email = request.form['ContactEmail']
+        name = request.form['Name']
+        content = request.form['contentMessage']
+        DBcontacts.add_inquiry(email, name, content)
         return render_template('/contact_us.html', message='תודה! הטופס נשלח בהצלחה.')
     else:
         return render_template('/contact_us.html')
